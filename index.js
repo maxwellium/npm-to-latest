550957637e9374e8a445a02b362a50435bcc3f46
maxwellium <x>
2020-01-12 13:17:49 +0100

    prevent update errors for linked and git deps; check for existing updates via npm outdated

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 #!/usr/bin/env node
 
-const { readFile } = require( 'fs' ),
-  { join } = require( 'path' );
+const { readFile } = require('fs').promises;
+const { join } = require('path');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
 
-readFile( join( process.cwd(), 'package.json' ), 'utf8', ( err, data ) => {
 
-  const { dependencies, devDependencies } = JSON.parse( data );
+async function main (cwd) {
 
-  if ( dependencies ) {
-    console.log( '# dependencies' );
-    console.log( `npm i -S ${ Object.keys( dependencies ).map( d => `${ d }@latest` ).join( ' ' ) }` );
+  let outdatedJson = '{}';
+
+  try {
+    const { stdout } = await exec('npm outdated --json', { cwd });
+    outdatedJson = stdout;
+  } catch (e) {
+    outdatedJson = e.stdout;
   }
 
-  if ( devDependencies ) {
-    console.log( '# devDependencies' );
-    console.log( `npm i -D ${ Object.keys( devDependencies ).map( d => `${ d }@latest` ).join( ' ' ) }` );
+  const packageJson = await readFile(join(cwd, 'package.json'), 'utf8')
+  const outdated = JSON.parse(outdatedJson);
+  const package = JSON.parse(packageJson);
+
+  const dependencyKeys = Object.keys(package.dependencies || {});
+  const devDependencyKeys = Object.keys(package.devDependencies || {});
+
+  const update = { dependencies: [], devDependencies: [] };
+
+  for (const key in outdated) {
+    if (!outdated[ key ]) { continue; }
+
+    if ([ 'linked', 'git' ].includes(outdated[ key ].latest)) { continue; }
+
+    if (dependencyKeys.includes(key)) {
+      update.dependencies.push(key)
+    }
+
+    if (devDependencyKeys.includes(key)) {
+      update.devDependencies.push(key)
+    }
   }
 
-} );
+  if (update.dependencies.length) {
+    console.log('# dependencies');
+    console.log(`npm i -S ${ update.dependencies.map(d => `${ d }@latest`).join(' ') }`);
+  }
+
+  if (update.devDependencies.length) {
+    console.log('# devDependencies');
+    console.log(`npm i -D ${ update.devDependencies.map(d => `${ d }@latest`).join(' ') }`);
+  }
+
+}
+
+
+main(process.cwd());
